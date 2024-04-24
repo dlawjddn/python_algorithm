3f1e40335e45de03f3fbcf6490187ea3903461bb
Lim jeong woo <dlawjddn0105@gmail.com>
2024-04-24 23:09:06 +0900

    단어 뒤집기2

---
+++ boj_17413.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+string = input()
+stack = list()
+ans = list()
+check_point = False
+for i in string:
+    if check_point:
+        if i == '>':
+            ans.append(i)
+            check_point = False
+        else:
+            ans.append(i)
+    else:
+        if i == '<':
+            while stack:
+                ans.append(stack.pop())
+            ans.append(i)
+            check_point = True
+        elif i == ' ':
+            while stack:
+                ans.append(stack.pop())
+            ans.append(i)
+        else:
+            stack.append(i)
+while stack:
+    ans.append(stack.pop())
+print(''.join(ans))
